4fce27db908164d04e9ea7dab9f25bc33bc531eb
MuhammadhamzaDS <hamzaali709x@gmail.com>
2025-09-11 08:53:38 +0500

    # Beginner-friendly Python programs

---
+++ basics/Day1.py
@@ -0,0 +1,28 @@
+# Python Intro Code
+
+# 1️⃣ Print Statement
+print("Hello, World!")   # Screen par output dikhata hai
+
+# 2️⃣ Variables
+name = "Hamza"           # String variable
+age = 21                 # Integer variable
+height = 5.9             # Float variable
+is_student = True        # Boolean variable
+
+# 3️⃣ Print Variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+# 4️⃣ Basic Arithmetic
+a = 10
+b = 5
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+# 5️⃣ Simple Input
+user_name = input("Enter your name: ")
+print("Welcome,", user_name)
