7a636f8790ed838a5a6aa421bb384c6d834ebd70
MuhammadhamzaDS <hamzaali709x@gmail.com>
2025-09-13 12:00:58 +0500

    Update

--- basics/Day1.py
+++ basics/Day1.py
@@ -1,28 +1,35 @@
-# Python Intro Code
+# # Python Intro Code
 
-# 1️⃣ Print Statement
-print("Hello, World!")   # Screen par output dikhata hai
+# # 1️⃣ Print Statement
+# print("Hello, World!")   # Screen par output dikhata hai
 
-# 2️⃣ Variables
-name = "Hamza"           # String variable
-age = 21                 # Integer variable
-height = 5.9             # Float variable
-is_student = True        # Boolean variable
+# # 2️⃣ Variables
+# name = "Hamza"           # String variable
+# age = 21                 # Integer variable
+# height = 5.9             # Float variable
+# is_student = True        # Boolean variable
 
-# 3️⃣ Print Variables
-print("Name:", name)
-print("Age:", age)
-print("Height:", height)
-print("Student:", is_student)
+# # 3️⃣ Print Variables
+# print("Name:", name)
+# print("Age:", age)
+# print("Height:", height)
+# print("Student:", is_student)
 
-# 4️⃣ Basic Arithmetic
-a = 10
-b = 5
-print("Addition:", a + b)
-print("Subtraction:", a - b)
-print("Multiplication:", a * b)
-print("Division:", a / b)
+# # 4️⃣ Basic Arithmetic
+# a = 10
+# b = 5
+# print("Addition:", a + b)
+# print("Subtraction:", a - b)
+# print("Multiplication:", a * b)
+# print("Division:", a / b)
 
-# 5️⃣ Simple Input
-user_name = input("Enter your name: ")
-print("Welcome,", user_name)
+# # 5️⃣ Simple Input
+# user_name = input("Enter your name: ")
+# print("Welcome,", user_name)
+
+print("Assalamo Alaikum")
+name = "Hamza"
+age = 22
+print(name,age)
+is_student = True
+print(is_student)
