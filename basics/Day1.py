487301ab04da40dd81e51aadc084fc61682f56dd
MuhammadhamzaDS <hamzaali709x@gmail.com>
2025-09-14 23:10:04 +0500

    Update

--- basics/Day1.py
+++ basics/Day1.py
@@ -1,21 +1,21 @@
-# spam_amount = 0
-# print (spam_amount)
-# spam_amount = spam_amount + 8
-# if spam_amount > 0:
-#     print("MY Name is Hamza,And I am a Python Developer")
-#     count = "Hamza" * spam_amount
-#     print(count)
+spam_amount = 0
+print (spam_amount)
+spam_amount = spam_amount + 8
+if spam_amount > 0:
+    print("MY Name is Hamza,And I am a Python Developer")
+    count = "Hamza" * spam_amount
+    print(count)
     
-# print(type(spam_amount))
-# print(type(19.89))
-# hat_height = 25.4
-# hat_width = 10.8
-# # without parenthesis Answer is different
-# total_area = hat_height + hat_width /2 
-# print(total_area)
-# # with parenthesis
-# total_area = (hat_height + hat_width) /2
-# print(total_area)
+print(type(spam_amount))
+print(type(19.89))
+hat_height = 25.4
+hat_width = 10.8
+# without parenthesis Answer is different
+total_area = hat_height + hat_width /2 
+print(total_area)
+# with parenthesis
+total_area = (hat_height + hat_width) /2
+print(total_area)
 print(min(1, 2, 3))
 print(max(1, 2, 3))
 print(abs(-9.8))
