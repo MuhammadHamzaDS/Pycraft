d6314ca8ac875c5807bde1f69a544d87ea377e3f
MuhammadhamzaDS <hamzaali709x@gmail.com>
2025-09-16 21:31:03 +0500

    Updated

--- basics/Day2.py
+++ basics/Day2.py
@@ -1,3 +1,10 @@
 help(round)
 mystery = print()
 print(mystery)
+
+num = int(input("Enter a number: "))
+
+# Table print karna
+print(f"\nTable of {num}:")
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
