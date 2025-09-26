8bb0948a940d3dfb7a13fdff613370ab4aec7873
MuhammadHamzaDS <hamzaali709x@gmail.com>
2025-09-26 00:58:16 -0700

    Update Decorator

--- basics/decorator.py
+++ basics/decorator.py
@@ -1,14 +1,27 @@
-# decorator
-def clean_table(func):
-    def wrapper():
-        print("👉 Cup saaf kar rahe hain")
-        func()   # asal function chalega
-        print("👉 Table saaf kar rahe hain")
+# # decorator
+# def clean_table(func):
+#     def wrapper():
+#         print("👉 Cup saaf kar rahe hain")
+#         func()   # asal function chalega
+#         print("👉 Table saaf kar rahe hain")
+#     return wrapper
+
+# # asal function
+# @clean_table
+# def make_tea():
+#     print("🍵 Chai ban rahi hai")
+
+# make_tea()
+# built-in functionsc
+def hello(func):
+    def wrapper ():
+        print("Hello Hamza Bhai")
+        func()
+        print("Hello Hamza Bhai Ye End Statement Hai")
     return wrapper
 
-# asal function
-@clean_table
-def make_tea():
-    print("🍵 Chai ban rahi hai")
+@hello 
+def hello():
+    print("Main Code hai ye wala")
 
-make_tea()
+hello()
