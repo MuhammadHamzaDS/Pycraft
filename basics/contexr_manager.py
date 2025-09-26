7e0448a1171eac454f36ef1f65c7d5200d96d1ce
MuhammadHamzaDS <hamzaali709x@gmail.com>
2025-09-26 04:10:33 -0700

    Update

---
+++ basics/contexr_manager.py
@@ -0,0 +1,20 @@
+# with open("data.txt", "r") as f:
+#     data = f.read()
+#     print(data)
+# # Write
+with open("data.txt", "w") as f:
+    f.write("Hello Hamza!\n")
+    f.write("This is written using context manager.\n")
+
+# Read
+with open("data.txt", "r") as f:
+    data = f.read()
+    print(data)
+
+with open("data.txt", "w") as f:
+    f.write("My Name is Muhammad Hamza\n")
+    f.write("SP23-BCS-093")
+
+with open("data.txt","r") as f:
+    data = f.read()
+    print (data)
