4400fb0be2974faf13a2eb84ac3920239ebeb0ef
MuhammadHamzaDS <hamzaali709x@gmail.com>
2025-10-23 06:09:40 +0500

    Updated

--- basics/decorator.py
+++ basics/decorator.py
@@ -1,17 +1,18 @@
-# # decorator
-# def clean_table(func):
-#     def wrapper():
-#         print("👉 Cup saaf kar rahe hain")
-#         func()   # asal function chalega
-#         print("👉 Table saaf kar rahe hain")
-#     return wrapper
+# decorator:Function ki functionality ko modify karne ka ek tareeqa hai bina us function ke code ko badle
+# decorator ek function ko leta hai aur ek naya function return karta hai jo asal function ki functionality ko enhance ya modify karta hai
+def clean_table(func):
+    def wrapper():
+        print("👉 Cup saaf kar rahe hain")
+        func()   # asal function chalega
+        print("👉 Table saaf kar rahe hain")
+    return wrapper
 
-# # asal function
-# @clean_table
-# def make_tea():
-#     print("🍵 Chai ban rahi hai")
+# asal function
+@clean_table
+def make_tea():
+    print("🍵 Chai ban rahi hai")
 
-# make_tea()
+make_tea()
 # built-in functionsc
 def hello(func):
     def wrapper ():
